80a52f7c5cc8f61a63ecf4063ce4c4391d2a8fcc
Benjamin Abt <mail@benjamin-abt.com>
2017-01-30 20:32:43 +0100

    Fixed typo

---
+++ src/ViewComponents/OpenGraphViewComponent.cs
@@ -0,0 +1,44 @@
+﻿using downr.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace downr.ViewComponents
+{
+    public class OpenGraphViewComponent : ViewComponent
+    {
+        private readonly DownrOptions _downrOptions;
+
+        public OpenGraphViewComponent(IOptions<DownrOptions> options)
+        {
+            _downrOptions = options.Value;
+        }
+
+        public IViewComponentResult Invoke(Metadata metadata = null)
+        {
+            MetadataViewComponentModel model;
+
+            // defaults
+            if (metadata == null)
+            {
+                model = new MetadataViewComponentModel
+                {
+                    Author = _downrOptions.Author,
+                    Description = _downrOptions.Description,
+                    Keywords = _downrOptions.Keywords
+                };
+            }
+            // post or page with metadata
+            else
+            {
+                model = new MetadataViewComponentModel
+                {
+                    Author = metadata.Author,
+                    Description = metadata.Description,
+                    Keywords = _downrOptions.Keywords
+                };
+            }
+
+            return View("Default", model);
+        }
+    }
+}
---
+++ src/ViewComponents/OpenGraphViewComponentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace downr.ViewComponents
+{
+    public class OpenGraphViewComponentModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Keywords { get; set; }
+    }
+}
