6e2a3edef1215d7b87a9d713d535a0df6c811aaa
Benjamin Abt <mail@benjamin-abt.com>
2017-03-06 20:58:53 +0100

    Merged error behavior

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -13,5 +13,25 @@ namespace downr.Controllers
         {
             return RedirectToAction("Page", "Downr", new {slug = "home"});
         }
+
+        [Route("error/{0}")]
+        public IActionResult Error(int errorCode)
+        {
+            ViewBag.ErrorCode = errorCode;
+
+            return View();
+        }
+
+        [Route("error/404")]
+        public IActionResult PageNotFound()
+        {
+            return View();
+        }
+
+        [Route("error/500")]
+        public IActionResult InternalError()
+        {
+            return View();
+        }
     }
 }
--- src/Startup.cs
+++ src/Startup.cs
@@ -65,7 +65,7 @@ namespace downr
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithReExecute("/error/{0}");
             }
 
             app.UseResponseCompression();
