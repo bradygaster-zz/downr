e2bb166796fb69c3eb3b7a2d1dae65ad0c370dce
Stuart Leeks <stuart@leeks.net>
2017-08-24 20:11:24 +0100

    Add CSS minification

--- src/gruntfile.js
+++ src/gruntfile.js
@@ -27,29 +27,40 @@ module.exports = function (grunt) {
                 }]
             }
         },
+        cssmin: {
+            style: {
+                files: [{
+                    expand: true,
+                    cwd: '..//css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'wwwroot/css',
+                    ext: '.min.css'
+                }]
+            }
+        },
         clean: {
             posts: 'wwwroot/posts',
             style: 'wwwroot/css'
         },
-        imagemin: {                            
-            dynamic: {                         
-            files: [{
-                expand: true,                  // Enable dynamic expansion
-                cwd: '../posts',               // Src matches are relative to this path
-                src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
-                dest: 'wwwroot/posts'          // Destination path prefix
-            }]
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: '../posts',               // Src matches are relative to this path
+                    src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'wwwroot/posts'          // Destination path prefix
+                }]
             }
         }
     });
 
     grunt.registerTask('postpublish', ['clean:posts', 'copy:content', 'imagemin']);
 
-    grunt.registerTask('precompile', ['copy:views', 'clean:style', 'copy:style']);
+    grunt.registerTask('precompile', ['copy:views', 'clean:style', 'cssmin:style']);
 
     grunt.loadNpmTasks('grunt-contrib-copy');
-
     grunt.loadNpmTasks('grunt-contrib-clean');
-
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
+
 };
